91abde7c2a223522575911ea5c4425277b66d083
Petrik <alex.petrovich83@gmail.com>
2022-01-25 11:48:12 +0300

    Initial Commit

---
+++ HW16/HW16App.swift
@@ -0,0 +1,17 @@
+//
+//  HW16App.swift
+//  HW16
+//
+//  Created by Aliaksandr Piatrovich on 25.01.22.
+//
+
+import SwiftUI
+
+@main
+struct HW16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
