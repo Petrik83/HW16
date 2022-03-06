51a3f8c760cc30943e332f2d7e45a0872d43140e
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2022-03-06 10:01:46 +0300

    Remade CategoryView.

--- HW16/Sourses/Scenes/FindView/StartScreen/CategoryView/CategoryView.swift
+++ HW16/Sourses/Scenes/FindView/StartScreen/CategoryView/CategoryView.swift
@@ -8,105 +8,134 @@
 import SwiftUI
 
 struct CategoryView: View {
-    
-    @Environment(\.presentationMode) var presentation
-    var categoryItem: CategoryViewItem
     var title = String()
-    let rows = [GridItem(.flexible())]
-    let secondRows = [GridItem(.fixed(UIScreen.main.bounds.width * CategoryViewMetric.secondRowsScale)),
-                      GridItem(.fixed(UIScreen.main.bounds.width * CategoryViewMetric.secondRowsScale))]
-    let thirdRows: [GridItem] = Array(repeating: .init(.fixed(CategoryViewMetric.thirdRowsHeight)), count: CategoryViewMetric.thirdRowsCount)
+    var categoryItem: CategoryViewItem
     
     var body: some View {
         VStack(alignment: .leading) {
-            HStack {
-                Button {
-                    self.presentation.wrappedValue.dismiss()
-                } label: {
-                    Image(systemName: "chevron.backward")
-                        .foregroundColor(.red)
-                        .font(.title)
-                }
-                Spacer()
-                Button {
-                    print("Нажата кнопка")
-                } label: {
-                    Image(systemName: "ellipsis.circle.fill")
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(Color.red, Color(uiColor: .systemGray6))
-                        .font(.title)
-                }
-            }
-            .padding(.horizontal, CategoryViewMetric.buttonsPadding)
-            Text(title)
-                .font(.title.bold())
-                .padding(.bottom, CategoryViewMetric.titleTextPaddingBottom)
-                .padding(.leading, CategoryViewMetric.titleTextPaddingLeading)
-            
-            Divider()
+            NavBar(title: title)
             
             ScrollView(.vertical, showsIndicators: false) {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHGrid(rows: rows) {
-                        ForEach(0..<categoryItem.firstSectionItem.count) {index in
-                            MainHorizontalCell(mainHorizontalCellItem: categoryItem.firstSectionItem[index])
-                                .frame(width: UIScreen.main.bounds.width * CategoryViewMetric.firstLazyHGridScale, height: UIScreen.main.bounds.width * CategoryViewMetric.firstLazyHGridScale)
-                        }
-                    }
-                }
+                FirstSectionScrollView(item: categoryItem.firstSectionItem)
                 Divider()
+                SectionDivider(title: "Плейлисты")
                 
-                HStack {
-                    Text("Плейлисты")
-                        .font(.title.bold())
-                    Spacer()
-                    Button {
-                        print("See all buttton pressed")
-                    } label: {
-                        Text("См. все")
-                            .foregroundColor(.red)
-                    }
-                }
-                .padding(.horizontal)
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHGrid(rows: secondRows, spacing: 0) {
-                        ForEach(0..<categoryItem.secondSectionItem.count) {index in
-                            SecondaryHorizontalCell(secondaryHorizontalCellItem: categoryItem.secondSectionItem[index])
-                                .frame(width: UIScreen.main.bounds.width * CategoryViewMetric.secondLazyHGridScale)
-                        }
-                    }
-                }
+                SecondSectionScrollView(item: categoryItem.secondSectionItem)
                 Divider()
+                SectionDivider(title: "Горячие треки")
                 
-                HStack {
-                    Text("Горячие треки")
-                        .font(.title.bold())
-                    Spacer()
-                    Button {
-                        print("See all buttton pressed")
-                    } label: {
-                        Text("См. все")
-                            .foregroundColor(.red)
-                    }
-                }
-                .padding(.horizontal)
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHGrid(rows: thirdRows, spacing: CategoryViewMetric.thirdLazyHGridSpacing) {
-                        ForEach(0..<categoryItem.therdSectionItem.count) { index in
-                            ThirdHorizontalCell(thirdHorizontalCellItem: categoryItem.therdSectionItem[index])
-                                .frame(width: UIScreen.main.bounds.width * CategoryViewMetric.thirdLazyHGridScale)
-                        }
-                    }
-                }
-                .padding(.bottom, CategoryViewMetric.thirdLazyHGridPadding)
+                ThirdSectionScrollView(item: categoryItem.therdSectionItem)
             }
         }
     }
 }
 
+struct NavBar: View {
+    var title = String()
+    @Environment(\.presentationMode) var presentation
+
+    var body: some View {
+        HStack {
+            Button {
+                self.presentation.wrappedValue.dismiss()
+            } label: {
+                Image(systemName: "chevron.backward")
+                    .foregroundColor(.red)
+                    .font(.title)
+            }
+            Spacer()
+            Button {
+                print("Нажата кнопка")
+            } label: {
+                Image(systemName: "ellipsis.circle.fill")
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(Color.red, Color(uiColor: .systemGray6))
+                    .font(.title)
+            }
+        }
+        .padding(.horizontal, CategoryViewMetric.buttonsPadding)
+        
+        Text(title)
+            .font(.title.bold())
+            .padding(.bottom, CategoryViewMetric.titleTextPaddingBottom)
+            .padding(.leading, CategoryViewMetric.titleTextPaddingLeading)
+        
+        Divider()
+        
+    }
+}
+
+struct FirstSectionScrollView: View {
+    var item: [FirstSectionItem]
+    let rows = [GridItem(.flexible())]
+
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: rows) {
+                ForEach(0..<item.count) {index in
+                    MainHorizontalCell(mainHorizontalCellItem: item[index])
+                        .frame(width: UIScreen.main.bounds.width * CategoryViewMetric.firstLazyHGridScale, height: UIScreen.main.bounds.width * CategoryViewMetric.firstLazyHGridScale)
+                }
+            }
+        }
+    }
+}
+
+struct SectionDivider: View {
+    var title = String()
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.title.bold())
+            Spacer()
+            Button {
+                print("See all buttton pressed")
+            } label: {
+                Text("См. все")
+                    .foregroundColor(.red)
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct SecondSectionScrollView: View {
+    var item: [SectionItem]
+    let secondRows = [GridItem(.fixed(UIScreen.main.bounds.width * CategoryViewMetric.secondRowsScale)),
+                      GridItem(.fixed(UIScreen.main.bounds.width * CategoryViewMetric.secondRowsScale))]
+
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: secondRows, spacing: 0) {
+                ForEach(0..<item.count) {index in
+                    SecondaryHorizontalCell(secondaryHorizontalCellItem: item[index])
+                        .frame(width: UIScreen.main.bounds.width * CategoryViewMetric.secondLazyHGridScale)
+                }
+            }
+        }
+    }
+}
+
+struct ThirdSectionScrollView: View {
+    var item: [SectionItem]
+    let thirdRows: [GridItem] = Array(repeating: .init(.fixed(CategoryViewMetric.thirdRowsHeight)), count: CategoryViewMetric.thirdRowsCount)
+
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: thirdRows, spacing: CategoryViewMetric.thirdLazyHGridSpacing) {
+                ForEach(0..<item.count) { index in
+                    ThirdHorizontalCell(thirdHorizontalCellItem: item[index])
+                        .frame(width: UIScreen.main.bounds.width * CategoryViewMetric.thirdLazyHGridScale)
+                }
+            }
+        }
+        .padding(.bottom, CategoryViewMetric.thirdLazyHGridPadding)
+    }
+}
+
 enum CategoryViewMetric {
     static let secondRowsScale = 0.53
-
+    
     static let thirdRowsHeight = 60.0
     static let thirdRowsCount = 4
     
