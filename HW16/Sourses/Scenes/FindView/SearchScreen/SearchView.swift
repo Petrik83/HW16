02a891fc51ccb4e922501cca2a12e223d943b576
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2022-03-06 10:49:05 +0300

    Remade SearchView

--- HW16/Sourses/Scenes/FindView/SearchScreen/SearchView.swift
+++ HW16/Sourses/Scenes/FindView/SearchScreen/SearchView.swift
@@ -28,60 +28,17 @@ struct SearchView: View {
             
             if searchText.searchText == "" {
                 if !searchText.lastSearch.isEmpty {
-                    HStack{
-                        Text("Недавние поиски")
-                            .font(Font.body.bold())
-                        Spacer()
-                        Button {
-                            searchText.lastSearch.removeAll()
-                        } label: {
-                            Text("Очистить")
-                                .foregroundColor(.red)
-                                .font(Font.body.bold())
-                        }
-                    }
-                    .padding(.horizontal, SearchViewMetric.horizontalPadding)
+                    LastSearchBar()
                 }
-                
-                List{
-                    ForEach (Array(Set(searchText.lastSearch)), id:\.self) { index in
-                        Button {
-                            searchText.searchResult = index
-                            playerPresenter.timerSlider = 0
-                            UIApplication.shared.endEditing(true)
-                        } label: {
-                            SearchResultCell(SearchResultCellItem: index)
-                        }
-                    }
-                }
-                .listStyle(.plain)
-                .gesture(DragGesture().onChanged{_ in UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)})
+                LastSearchList()
             } else {
                 List {
                     ForEach(searchHints(incomingData: FindViewItem.findViewItem, text: searchText.searchText), id:\.self) { index in
-                        Button {
-                            searchText.searchText = index
-                        } label: {
-                            HStack(spacing: SearchViewMetric.spacing) {
-                                Image(systemName: "magnifyingglass")
-                                    .padding(.trailing, SearchViewMetric.trailingPadding)
-                                Text(searchText.searchText)
-                                Text(index.dropFirst(searchText.searchText.count))
-                                    .foregroundColor(.secondary)
-                                Spacer()
-                            }
-                        }
+                        SearchHintsButton(item: index)
                     }
                     
                     ForEach (searchFilter(incomingData: FindViewItem.findViewItem, text: searchText.searchText), id:\.self) { index in
-                        Button {
-                            searchText.searchResult = index
-                            playerPresenter.timerSlider = 0
-                            searchText.lastSearch.append(index)
-                            UIApplication.shared.endEditing(true)
-                        } label: {
-                            SearchResultCell(SearchResultCellItem: index)
-                        }
+                        SearchFilterButton(item: index)
                     }
                 }
                 .listStyle(.plain)
@@ -122,9 +79,92 @@ struct SearchView: View {
     }
 }
 
+struct LastSearchBar: View {
+    @EnvironmentObject var searchText: SearchText
+    
+    var body: some View {
+        HStack{
+            Text("Недавние поиски")
+                .font(Font.body.bold())
+            Spacer()
+            Button {
+                searchText.lastSearch.removeAll()
+            } label: {
+                Text("Очистить")
+                    .foregroundColor(.red)
+                    .font(Font.body.bold())
+            }
+        }
+        .padding(.horizontal, SearchViewMetric.horizontalPadding)
+    }
+}
+
+struct LastSearchList: View {
+    @EnvironmentObject var searchText: SearchText
+    @EnvironmentObject var playerPresenter: PlayerPresenter
+    
+    var body: some View {
+        List{
+            ForEach (Array(Set(searchText.lastSearch)), id:\.self) { index in
+                Button {
+                    searchText.searchResult = index
+                    playerPresenter.timerSlider = 0
+                    UIApplication.shared.endEditing(true)
+                } label: {
+                    SearchResultCell(SearchResultCellItem: index)
+                }
+            }
+        }
+        .listStyle(.plain)
+        .gesture(DragGesture().onChanged{_ in UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)})
+    }
+}
+
+struct SearchHintsButton: View {
+    @EnvironmentObject var searchText: SearchText
+    var item = String()
+    
+    var body: some View {
+        
+        Button {
+            searchText.searchText = item
+        } label: {
+            HStack(spacing: SearchViewMetric.spacing) {
+                Image(systemName: "magnifyingglass")
+                    .padding(.trailing, SearchViewMetric.trailingPadding)
+                Text(searchText.searchText)
+                Text(item.dropFirst(searchText.searchText.count))
+                    .foregroundColor(.secondary)
+                Spacer()
+            }
+        }
+    }
+    
+}
+
+struct SearchFilterButton: View {
+    @EnvironmentObject var searchText: SearchText
+    @EnvironmentObject var playerPresenter: PlayerPresenter
+    var item: SectionItem
+    
+    var body: some View {
+        
+        Button {
+            searchText.searchResult = item
+            playerPresenter.timerSlider = 0
+            searchText.lastSearch.append(item)
+            UIApplication.shared.endEditing(true)
+        } label: {
+            SearchResultCell(SearchResultCellItem: item)
+            
+        }
+    }
+    
+}
+
 enum SearchViewMetric {
     static let horizontalPadding = 20.0
     static let trailingPadding = 10.0
     static let spacing = 0.0
-
+    
 }
